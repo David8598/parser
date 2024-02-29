d87dcc38b58e2d5cc8463dd7e70019e5130abd11
arturious9090 <sefer07artur@gmail.com>
2024-02-29 22:50:41 +0200

    19.02.2024 создаем ответ для тг

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import json
 import requests
+from datetime import datetime
+
+
+def get_message(shots_target):             #создаем ответ в тг 
+    timestemp = shots_target['S'] + 60*60
+    game_date = datetime.fromtimestamp(timestemp)
+    print(game_date)
 
 def get_SHOTS_TARGET(game_result):
-    for game in game_result['Value']:
-        country = game["CN"]
-        league = game["L"]
-        comand1 = game["O1"]
-        comand2 = game["O2"]
+    for game in game_result['Value']:  
+        shots_target = {}                  #здесь храняться все файлы которые получили из json игры 
+        shots_target['CN'] = game["CN"]
+        shots_target['L']= game["L"]
+        shots_target['COM1'] = game["O1"]
+        shots_target['COM2'] = game["O2"]
+        shots_target['S'] = game['S']
+                
         try:
-            bets = game["SC"]["ST"]
+            bets = game["SC"]["ST"]         #парсим json который получили 
             for item in bets:
                 match_info = item["Value"]
                 for SHOTS_TARGET in match_info:
@@ -17,18 +27,25 @@ def get_SHOTS_TARGET(game_result):
                         SHOTS_on_TARGET_S2 = SHOTS_TARGET["S2"] 
                     if SHOTS_TARGET["ID"] == 60:
                         SHOTS_off_TARGET_S1 = SHOTS_TARGET["S1"]
-                        SHOTS_off_TARGET_S2 = SHOTS_TARGET["S2"] 
+                        SHOTS_off_TARGET_S2 = SHOTS_TARGET["S2"]
+                shots_target['ONS1'] = SHOTS_on_TARGET_S1
+                shots_target['ONS2'] = SHOTS_on_TARGET_S2
+                shots_target['OFFS1'] = SHOTS_off_TARGET_S1
+                shots_target['OFFS2'] = SHOTS_off_TARGET_S2         
         except:
-            break
+            break 
         try:
-            shots_On_num1 = int(SHOTS_on_TARGET_S1)
-            shots_On_num2 = int(SHOTS_on_TARGET_S2)
-            print(abs(shots_On_num1 - shots_On_num2))
-            # print( f"============\n{country}\n============\n{league}\n=============\n{comand1} vs {comand2}\n=============\nSHOTS_on_TARGET:\n{SHOTS_on_TARGET_S1} ======== {SHOTS_on_TARGET_S2} \n SHOTS_off_TARGET_:\n{SHOTS_off_TARGET_S1}  ======== {SHOTS_off_TARGET_S2}" )             
-        except:  break 
+            shots_On_numS1 = int(SHOTS_on_TARGET_S1)
+            shots_On_numS2 = int(SHOTS_on_TARGET_S2)
+            difference = abs(shots_On_numS1 - shots_On_numS2)  
+            shots_target['DIFf'] = difference
+        except:  
+            break
+    get_message(shots_target)
+
         
 
-def get_game(result):
+def get_game(result):                         # проходимся по всем матчам 
     for game in result['Value']:
         champs = game['LI']
         # print(champs)
@@ -47,9 +64,10 @@ def get_game(result):
         response = requests.get('https://1xbit6.com/LiveFeed/Get1x2_VZip', params=params)
         game_result = response.json()
         get_SHOTS_TARGET(game_result)
+        break
         
 
-def main():
+def main(): #достаем json с сайта 
     params = (
     ('sports', '1'),  
     ('count', '50'),
