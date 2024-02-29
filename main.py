98599cb68fe4023af1d3a05b0bf375d4871f4a0a
arturious9090 <sefer07artur@gmail.com>
2024-02-29 14:25:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,71 @@
+import json
 import requests
 
-# headers= {
-#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-#     ,"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-# }
-url = "https://1xbit-ua.com/LiveFeed/Get1x2_VZip?sports=1&count=50&gr=29&mode=4&country=2&partner=65&getEmpty=true"
-champs = url.split('/')
-print(champs)
-params = {
-    'sports': '1',
-    'champs': '2048450',
-    'lng': 'en',
-    'gr': '29',
-    'country': '2',
-    'partner': '65',
-    'virtualSports': 'true',
-    'groupChamps': 'true',
-}
-    
 
-req = requests.get(url, headers=params)
-result = req.json()
-# print(result)
-# print(src)
+def get_SHOTS_ON_TARGET(game_result, game_id):
+    for game in game_result['Value']:
+        currend_id = game['I']
+        if currend_id == game_id:
+            bets = game["SC"]["ST"]
+            for item in bets:
+            
+        
+
+
+
+
+def get_game(result):
+    
+    for game in result['Value']:
+        game_id = game['I']
+        champs = game['LI']
+
+        params = (
+            ('sports', '1'),
+            ('champs', champs),
+            ('count', '50'),
+            ('gr', '29'),
+            ('mode', '4'),
+            ('subGames', game_id),
+            ('country', '2'),
+            ('partner', '65'),      
+            ('getEmpty', 'true'),
+            )
+
+        response = requests.get('https://1xbit6.com/LiveFeed/Get1x2_VZip', params=params)
+        game_result = response.json()
+        get_SHOTS_ON_TARGET(game_result , game_id)
+
+        
+        break
+
+        
+        
+        
+
+
+def main():
+    url = 'https://1xbit6.com/live/football/1134147-friendlies-u19-national-teams'
+    champs = url.split('/')[5].split('-')[0]
+    # print(champs)
+
+    params = (
+    ('sports', '1'),
+    ('champs', champs),
+    ('count', '50'),
+    ('gr', '29'),
+    ('mode', '4'),
+    ('country', '2'),
+    ('partner', '65'),
+    ('getEmpty', 'true'),
+    )
+
+    response = requests.get('https://1xbit6.com/LiveFeed/Get1x2_VZip', params=params)
+    result = response.json()
+    get_game(result)
+
+
+
+if __name__ == '__main__':
+    main()
     
