cc314f7e9c2cfcce4794395a569cda7b7c8f24dc
arturious9090 <sefer07artur@gmail.com>
2024-03-01 18:38:37 +0200

    сортировка

--- main.py
+++ main.py
@@ -13,27 +13,32 @@ def get_message(shots_target):             #создаем ответ в тг
                 f'{team_1} - {team_2}\n' \
                 f'\n' \
                 f'on {shots_on_T1} - {shots_on_T2}\n' \
-                f'off {shots_off_T1} - {shots_off_T2}'     
-   
+                f'off {shots_off_T1} - {shots_off_T2}'        
     send_telegram(massege)            
     
                 
+     
 # def search_db(game_id, shots_target):
-#     with open ('db.txt', 'r' ) as file:
+#     with open ('db.txt', 'r'  ) as file:
 #         for item in file.readlines():
 #             line = item.strip()
-#             if int(float(line)) != game_id:
-#                 print("id со списка" + int(float(line)))
-#                 print("id с запроса" + game_id)
+#             if int(line) != game_id:
 #                 get_message(shots_target)
-#     with open ('db.txt', 'a' ) as file:
-#         file.write(f'\n{game_id}')     
-def search_db(game_id, shots_target):
-    with open ('db.txt', 'r' ) as file:
-        if file.read() != game_id:
-            get_message(shots_target)
-            with open ('db.txt', 'a' ) as f:
-                f.write(f'\n{game_id}')
+#         with open ('db.txt', 'a' ) as file:
+#             file.write(f'\n{game_id}')
+
+
+
+
+def sort_data(shots_target):
+    try:
+        if abs(int(shots_target['ONS1']) - int(shots_target['ONS2'])) and abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2'])) >= 4: 
+            print(abs(int(shots_target['ONS1']) - int(shots_target['ONS2'])))
+    except:
+        print(shots_target['I'])
+            # abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2']))
+        # diference_on_target = abs(int(shots_target['ONS1']) - int(shots_target['ONS2']))
+        # diference_off_target = abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2']))
 
 
 def get_SHOTS_TARGET(game_result):
@@ -44,33 +49,27 @@ def get_SHOTS_TARGET(game_result):
         shots_target['COM1'] = game["O1"]
         shots_target['COM2'] = game["O2"]
         shots_target['S'] = game['S']
-                
+        shots_target['I'] = game['I']
         try:
             bets = game["SC"]["ST"]         #парсим json который получили 
             for item in bets:
                 match_info = item["Value"]
                 for SHOTS_TARGET in match_info:
-                    if SHOTS_TARGET["ID"] == 59:
-                        SHOTS_on_TARGET_S1 = SHOTS_TARGET["S1"]
-                        SHOTS_on_TARGET_S2 = SHOTS_TARGET["S2"] 
-                    if SHOTS_TARGET["ID"] == 60:
-                        SHOTS_off_TARGET_S1 = SHOTS_TARGET["S1"]
-                        SHOTS_off_TARGET_S2 = SHOTS_TARGET["S2"]
-                shots_target['ONS1'] = SHOTS_on_TARGET_S1
-                shots_target['ONS2'] = SHOTS_on_TARGET_S2
-                shots_target['OFFS1'] = SHOTS_off_TARGET_S1
-                shots_target['OFFS2'] = SHOTS_off_TARGET_S2         
+                    try:
+                        if SHOTS_TARGET["ID"] == 59:
+                            shots_target['ONS1'] = SHOTS_TARGET["S1"]
+                            shots_target['ONS2'] = SHOTS_TARGET["S2"] 
+                        if SHOTS_TARGET["ID"] == 60:
+                            shots_target['OFFS1'] = SHOTS_TARGET["S1"]
+                            shots_target['OFFS2'] = SHOTS_TARGET["S2"]
+                    except:
+                        if SHOTS_TARGET["ID"] == 59:
+                            shots_target['ONS1'] = SHOTS_TARGET["S1"]
+                            shots_target['ONS2'] = SHOTS_TARGET["S2"]
         except: 
-            print()
-        try:
-            shots_On_numS1 = int(SHOTS_on_TARGET_S1)
-            shots_On_numS2 = int(SHOTS_on_TARGET_S2)
-            difference = abs(shots_On_numS1 - shots_On_numS2)  
-            shots_target['DIFf'] = difference
-        except:  
-            print()
-        game_id = game['I']    
-        search_db(game_id, shots_target)
+            pass
+        sort_data(shots_target)
+        # search_db(game_id, shots_target)
 
         
 
