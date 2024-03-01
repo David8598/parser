fb4de663c2ef4fbfe59b012995105c7c230e1306
arturious9090 <sefer07artur@gmail.com>
2024-03-01 21:37:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import requests
 from datetime import datetime
 from telegram import send_telegram
 
-
 def get_message(shots_target):             #создаем ответ в тг 
     timestemp = shots_target['S']
     game_date = datetime.fromtimestamp(timestemp).strftime('%d.%m %H:%M')
-    country, league, team_1, team_2, time_S, id, shots_on_T1, shots_on_T2, shots_off_T1, shots_off_T2, dif = shots_target.values()
+    country, league, team_1, team_2, time_S, id, shots_on_T1, shots_on_T2, shots_off_T1, shots_off_T2, = shots_target.values()
     # формируем сообщение для тг 
     massege = f'{league}({game_date})\n' \
                 f'{team_1} - {team_2}\n' \
@@ -16,44 +15,18 @@ def get_message(shots_target):             #создаем ответ в тг
                 f'off {shots_off_T1} - {shots_off_T2}'        
     print(massege)
     send_telegram(massege)            
-    
-                
-     
-# def search_db(game_id, shots_target):
-#     with open ('db.txt', 'r'  ) as file:
-#         for item in file.readlines():
-#             line = item.strip()
-#             if int(line) != game_id:
-#                 get_message(shots_target)
-#         with open ('db.txt', 'a' ) as file:
-#             file.write(f'\n{game_id}')
-
-
-
 
 def sort_data(shots_target):
     try:
-        if abs(int(shots_target['ONS1']) - int(shots_target['ONS2']))  > 4: 
-            # print(abs(int(shots_target['ONS1']) - int(shots_target['ONS2'])))
-            # print(abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2'])))
+        if abs(int(shots_target['ONS1']) - int(shots_target['ONS2']))  >= 4: 
             get_message(shots_target)
-        elif abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2'])) > 4:
-            #  print(abs(int(shots_target['ONS1']) - int(shots_target['ONS2'])))
-            #  print(abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2'])))
+        elif abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2'])) >= 4:
              get_message(shots_target)
         else:
-            # print(shots_target['I'])
             pass
     except:
-        # print(shots_target['I'])
          pass
     
-
-            # abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2']))
-        # diference_on_target = abs(int(shots_target['ONS1']) - int(shots_target['ONS2']))
-        # diference_off_target = abs(int(shots_target['OFFS1']) - int(shots_target['OFFS2']))
-
-
 def get_SHOTS_TARGET(game_result):
         game = game_result['Value']
         shots_target = {}  
@@ -89,14 +62,11 @@ def get_SHOTS_TARGET(game_result):
         except: 
             pass
         sort_data(shots_target)
-        # search_db(game_id, shots_target)
-
         
 
 def get_game(result):                         # проходимся по всем матчам 
     for game in result['Value']:
         champs = game['I']
-        # print(champs)
 
         params = (
             ('id', champs),
@@ -104,7 +74,7 @@ def get_game(result):                         # проходимся по все
             ('isSubGames', 'true'),
             ('GroupEvents', 'true'),
             ('allEventsGroupSubGames', 'true'),
-           ('countevents', '250'),
+            ('countevents', '250'),
             ('partner', '65'),
             ('country', '2'),
             ('fcountry', '2'),
